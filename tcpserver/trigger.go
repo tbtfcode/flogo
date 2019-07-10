30d543e92c9c40081188a40937da65ca280b9c53
tbtfproject <tbtf.base.project@gmail.com>
2019-07-10 16:36:07 +0900

    Modified

--- tcpserver/trigger.go
+++ tcpserver/trigger.go
@@ -5,8 +5,16 @@ import (
 	"github.com/project-flogo/core/trigger"
 )
 
-// TCPTrigger is a stub for your Trigger implementation
-type TCPTrigger struct {
+/*
+var triggerMetadata = trigger.NewMetadata(&Settings{}, &HandlerSettings{}, &Output{})
+
+func init() {
+	_ = trigger.Register(&Trigger{}, &Factory{})
+}
+*/
+
+// Trigger is a stub for your Trigger implementation
+type Trigger struct {
 	logger   log.Logger
 	handlers []trigger.Handler
 	//	settings *Settings
@@ -14,22 +22,22 @@ type TCPTrigger struct {
 	config   *trigger.Config
 }
 
-// TCPTriggerFactory My Trigger factory
-type TCPTriggerFactory struct {
+// Factory My Trigger factory
+type Factory struct {
 }
 
 // Metadata implements trigger.Trigger.Metadata
-func (t *TCPTrigger) Metadata() *trigger.Metadata {
+func (t *Trigger) Metadata() *trigger.Metadata {
 	return t.metadata
 }
 
 // New Creates a new trigger instance for a given id
-func (t *TCPTriggerFactory) New(config *trigger.Config) (trigger.Trigger, error) {
-	return &TCPTrigger{config: config}, nil
+func (*Factory) New(config *trigger.Config) (trigger.Trigger, error) {
+	return &Trigger{config: config}, nil
 }
 
 // Initialize implements trigger.Init.Initialize
-func (t *TCPTrigger) Initialize(ctx trigger.InitContext) error {
+func (t *Trigger) Initialize(ctx trigger.InitContext) error {
 
 	t.logger = ctx.Logger()
 	t.handlers = ctx.GetHandlers()
@@ -40,14 +48,14 @@ func (t *TCPTrigger) Initialize(ctx trigger.InitContext) error {
 }
 
 // Start implements trigger.Trigger.Start
-func (t *TCPTrigger) Start() error {
+func (t *Trigger) Start() error {
 	// start the trigger
 	t.logger.Infof("start the trigger : %s", "")
 	return nil
 }
 
 // Stop implements trigger.Trigger.Start
-func (t *TCPTrigger) Stop() error {
+func (t *Trigger) Stop() error {
 	// stop the trigger
 	t.logger.Infof("stop the trigger : %s", "")
 	return nil
--- tcpserver/trigger_test.go
+++ tcpserver/trigger_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/project-flogo/core/trigger"
+	"github.com/stretchr/testify/assert"
 )
 
 func getJSONMetadata() string {
@@ -17,14 +18,14 @@ func getJSONMetadata() string {
 }
 
 const testConfig string = `{
-  "id": "mytrigger",
-  "ref": "github.com/project-flogo/contrib/trigger/tcp",
+  "id": "tcpserver",
+  "ref": "github.com/tbtfcode/flogo/tcpserver",
   "settings": {
 	"network": "tcp",
 	"host": "127.0.0.1",
 	"port": "8982"
   },
-"handlers": [
+  "handlers": [
     {
       "settings": {
       },
@@ -38,19 +39,25 @@ const testConfig string = `{
 func TestCreate(t *testing.T) {
 
 	// New factory
-	md := trigger.NewMetadata(getJSONMetadata())
-	f := NewFactory(md)
+	// md := trigger.NewMetadata(getJSONMetadata())
 
-	if f == nil {
-		t.Fail()
-	}
+	f := &Factory{}
 
-	// New Trigger
-	config := trigger.Config{}
-	json.Unmarshal([]byte(testConfig), config)
-	trg := f.New(&config)
+	config := &trigger.Config{}
+	err := json.Unmarshal([]byte(testConfig), config)
+	// assert.Nil(t, err)
 
-	if trg == nil {
-		t.Fail()
-	}
+	// actions := map[string]action.Action{"dummy": test.NewDummyAction(func() {
+	// 	//do nothing
+	// })}
+
+	// trg, err := test.InitTrigger(f, config, actions)
+	trg, err := f.New(config)
+	assert.Nil(t, err)
+	assert.NotNil(t, trg)
+
+	err = trg.Start()
+	assert.Nil(t, err)
+	err = trg.Stop()
+	assert.Nil(t, err)
 }
